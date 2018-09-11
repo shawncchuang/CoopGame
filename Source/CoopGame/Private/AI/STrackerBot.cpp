3a89fb4883b0ee533361a92cad67260d7bab0092
shawn.huang <shawncc.huang@gmail.com>
2018-09-11 17:54:53 +0800

    Add Take Damage support, pulse material on take damage, add self-destruct and explode when near player

--- Source/CoopGame/Private/AI/STrackerBot.cpp
+++ Source/CoopGame/Private/AI/STrackerBot.cpp
@@ -7,6 +7,9 @@
 #include "AI/Navigation/NavigationPath.h"
 #include "GameFramework/Character.h"
 #include "DrawDebugHelpers.h"
+#include "SHealthComponent.h"
+#include "SCharacter.h"
+#include "Components/SphereComponent.h"
 
 // Sets default values
 ASTrackerBot::ASTrackerBot()
@@ -19,9 +22,22 @@ ASTrackerBot::ASTrackerBot()
 	MeshComp->SetSimulatePhysics(true);
 	RootComponent = MeshComp;
 
-	bUseVelocityChange = false;
+	HealthComp = CreateDefaultSubobject<USHealthComponent>(TEXT("HealthComp"));
+	HealthComp->OnHealthChanged.AddDynamic(this, &ASTrackerBot::HandleTakeDamage);
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetSphereRadius(200);
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	SphereComp->SetupAttachment(RootComponent);
+
+	bUseVelocityChange = true;
 	MovementForce = 1000;
 	RequiredDistanceToTarget = 100;
+
+	ExplosionRadius = 200;
+	ExplosionDamage = 100;
 }
 
 // Called when the game starts or when spawned
@@ -30,13 +46,35 @@ void ASTrackerBot::BeginPlay()
 	Super::BeginPlay();
 
 	// Find initial move to
-	FVector NextPoint = GetNextPathPoint();
+	NextPathPoint = GetNextPathPoint();
+}
+
+void ASTrackerBot::HandleTakeDamage(USHealthComponent *OwningHealthComp, float Health, float HealthDelta, const class UDamageType *DamageType, class AController *InstigateBy, AActor *DamageCauser)
+{
+	// Explore on hitpoint == 0
+
+	// TODO: Pulse the material on hit
+	if (MatInst == nullptr)
+	{
+		MatInst = MeshComp->CreateAndSetMaterialInstanceDynamicFromMaterial(0, MeshComp->GetMaterial(0));
+	}
+	if (MatInst)
+	{
+		MatInst->SetScalarParameterValue("LastTimeDamageTaken", GetWorld()->TimeSeconds);
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("Health %s of %s"), *FString::SanitizeFloat(Health), *GetName());
+
+	if (Health <= 0.0f)
+	{
+		SelfDestruct();
+	}
 }
 
 FVector ASTrackerBot::GetNextPathPoint()
 {
 	// Hack, to get player location
-	ACharacter *PlayerPawn = UGameplayStatics::GetPlayerCharacter(this, 0);
+	AActor *PlayerPawn = UGameplayStatics::GetPlayerPawn(this, 0);
 
 	UNavigationPath *NavPath = UNavigationSystem::FindPathToActorSynchronously(this, GetActorLocation(), PlayerPawn);
 
@@ -48,6 +86,35 @@ FVector ASTrackerBot::GetNextPathPoint()
 
 	//Failed to find path
 	return GetActorLocation();
+}
+
+void ASTrackerBot::SelfDestruct()
+{
+
+	if (bExplode)
+	{
+		return;
+	}
+	bExplode = true;
+
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, GetActorLocation());
+
+	TArray<AActor *> IgnoredActors;
+	IgnoredActors.Add(this);
+
+	// Apply Damage
+	UGameplayStatics::ApplyRadialDamage(this, ExplosionDamage, GetActorLocation(), ExplosionRadius, nullptr, IgnoredActors, this, GetInstigatorController(), true);
+
+	DrawDebugSphere(GetWorld(), GetActorLocation(), ExplosionRadius, 12, FColor::Red, false, 2.0f, 0.0f, 1.0f);
+
+	// Ddelete Actor immediately
+	Destroy();
+}
+
+void ASTrackerBot::DamageSelf()
+{
+
+	UGameplayStatics::ApplyDamage(this, 20, GetInstigatorController(), this, nullptr);
 }
 
 // Called every frame
@@ -69,8 +136,6 @@ void ASTrackerBot::Tick(float DeltaTime)
 		// Keep moving towards next target
 		FVector ForceDirection = NextPathPoint - GetActorLocation();
 		ForceDirection.Normalize();
-
-		ForceDirection.Normalize();
 		ForceDirection *= MovementForce;
 		MeshComp->AddForce(ForceDirection, NAME_None, bUseVelocityChange);
 
@@ -78,4 +143,22 @@ void ASTrackerBot::Tick(float DeltaTime)
 	}
 
 	DrawDebugSphere(GetWorld(), NextPathPoint, 20, 12, FColor::Blue, false, 4.0f, 1.0f);
+}
+
+void ASTrackerBot::NotifyActorBeginOverlap(AActor *OtherActor)
+{
+	if (!bStartedSelfDestruction)
+	{
+		ASCharacter* PlayerPawn = Cast<ASCharacter>(OtherActor);
+
+		if (PlayerPawn)
+		{
+			// we overlaped with a player
+
+			// Start self destruction sequence
+			GetWorldTimerManager().SetTimer(TimerHandle_SelfDamage, this, &ASTrackerBot::DamageSelf, 0.5f, true, 0.0f);
+
+			bStartedSelfDestruction = true;
+		}
+	}
 }
--- Source/CoopGame/Public/AI/STrackerBot.h
+++ Source/CoopGame/Public/AI/STrackerBot.h
@@ -6,21 +6,33 @@
 #include "GameFramework/Pawn.h"
 #include "STrackerBot.generated.h"
 
+class USHealthComponent;
+class USphereComponent;
+
 UCLASS()
 class COOPGAME_API ASTrackerBot : public APawn
 {
 	GENERATED_BODY()
 
-  public:
+public:
 	// Sets default values for this pawn's properties
 	ASTrackerBot();
 
-  protected:
+protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
 	UStaticMeshComponent *MeshComp;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	USHealthComponent *HealthComp;
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+	USphereComponent *SphereComp;
+
+	UFUNCTION()
+	void HandleTakeDamage(USHealthComponent *OwningHealthComp, float Health, float HealthDelta, const class UDamageType *DamageType, class AController *InstigateBy, AActor *DamageCauser);
 
 	FVector GetNextPathPoint();
 
@@ -36,7 +48,31 @@ class COOPGAME_API ASTrackerBot : public APawn
 	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
 	float RequiredDistanceToTarget;
 
-  public:
+	// Dynamic material to pulse on change
+	UMaterialInstanceDynamic *MatInst;
+
+	void SelfDestruct();
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	UParticleSystem *ExplosionEffect;
+
+	bool bExplode;
+
+	bool bStartedSelfDestruction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	float ExplosionRadius;
+
+	UPROPERTY(EditDefaultsOnly, Category = "TrackerBot")
+	float ExplosionDamage;
+
+	FTimerHandle TimerHandle_SelfDamage;
+
+	void DamageSelf();
+
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+
+	virtual void NotifyActorBeginOverlap(AActor *OtherActor) override;
 };
